3161ce3286c042487725c1acf9190c42f98878a5
Yeonglong Ay <albert99302@gmail.com>
2026-02-02 16:31:47 -0500

    Update init_triggers.m

--- src/triggers/init_triggers.m
+++ src/triggers/init_triggers.m
@@ -11,23 +11,18 @@ function trig = init_triggers(params)
     trig.pulseWidthSec = params.triggers.pulseWidthSec;
     trig.codes = params.triggers.codes;
 
-    switch trig.mode
-        case "parallel"
-            trig.ioObj = io64;
-            status = io64(trig.ioObj);
-            if status ~= 0
-                error('io64 initialization failed (status=%d).', status);
-            end
-            trig.address = hex2dec(params.triggers.parallel.addressHex);
-            io64(trig.ioObj, trig.address, 0);
-
-        case "serial"
-            port = params.triggers.serial.port;
-            baud = params.triggers.serial.baud;
-            trig.serialHandle = IOPort('OpenSerialPort', port, sprintf('BaudRate=%d', baud));
-            IOPort('Flush', trig.serialHandle);
-
-        otherwise
-            error('Unknown trigger mode: %s', trig.mode);
+    if trig.mode ~= "parallel"
+        error('This build is configured for parallel TTL (io64).');
     end
+
+    trig.ioObj = io64;
+    status = io64(trig.ioObj);
+    if status ~= 0
+        error('io64 initialization failed (status=%d). Check driver/install.', status);
+    end
+
+    trig.address = hex2dec(params.triggers.parallel.addressHex);
+
+    % Reset
+    io64(trig.ioObj, trig.address, 0);
 end
