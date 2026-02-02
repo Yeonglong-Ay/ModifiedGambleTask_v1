839e54b647462cd6973acf605c1f90fd0077ee6e
Yeonglong Ay <albert99302@gmail.com>
2026-02-02 15:38:38 -0500

    Update load_params.m

--- src/load_params.m
+++ src/load_params.m
@@ -2,8 +2,14 @@ function params = load_params(jsonPath)
     txt = fileread(jsonPath);
     params = jsondecode(txt);
 
-    % Convenience: map keys to KbName codes
     params.keys.leftCode  = KbName(params.keys.left);
     params.keys.rightCode = KbName(params.keys.right);
     params.keys.quitCode  = KbName(params.keys.quit);
+
+    % practice proceed keys (space or s)
+    proceedList = params.keys.proceedPractice;
+    if ischar(proceedList) || isstring(proceedList)
+        proceedList = {char(proceedList)};
+    end
+    params.keys.proceedCodes = cellfun(@(k) KbName(k), proceedList);
 end
