2c4eb5b60e63fb8e298ed978f724f4ee65d86428
Yeonglong Ay <albert99302@gmail.com>
2026-02-02 16:32:08 -0500

    Update send_trigger.m

--- src/triggers/send_trigger.m
+++ src/triggers/send_trigger.m
@@ -2,20 +2,8 @@ function send_trigger(trig, code)
     if isempty(trig) || ~isfield(trig,'enabled') || ~trig.enabled
         return;
     end
-    if ~isnumeric(code)
-        error('Trigger code must be numeric.');
-    end
 
-    switch trig.mode
-        case "parallel"
-            io64(trig.ioObj, trig.address, code);
-            WaitSecs(trig.pulseWidthSec);
-            io64(trig.ioObj, trig.address, 0);
-
-        case "serial"
-            IOPort('Write', trig.serialHandle, uint8(code));
-
-        otherwise
-            % no-op
-    end
+    io64(trig.ioObj, trig.address, code);
+    WaitSecs(trig.pulseWidthSec);   % 0.005 sec is OK (your spec)
+    io64(trig.ioObj, trig.address, 0);
 end
